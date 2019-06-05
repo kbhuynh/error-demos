15d49274121d9e77d8ab9f1533306a934a6d23ff
Kevin Huynh <32022025+kbhuynh@users.noreply.github.com>
2019-06-05 13:13:14 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById("bm"),
+    renderer: 'svg',
+    load: true,
+    autoplay: true,
+    loop: true,
+    path: '/data-2.json'
+})
